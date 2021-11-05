9bebaec1f8c59c0b9597e28d7bc2d3dd94fc00d1
danex11 <danex11@op.pl>
2021-11-05 11:11:04 +0100

    Initial commit It freakin works.

---
+++ app/src/main/java/pl/op/danex11/minimalrewardedex1/MainActivity.java
@@ -0,0 +1,125 @@
+package pl.op.danex11.minimalrewardedex1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean initializationDone = false;
+    String TAG = "tag";
+    private RewardedAd mRewardedAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+
+        // initialize the Google Mobile Ads SDK
+        // initializes the SDK and calls back a completion listener once initialization is complete (or after a 30-second timeout).
+        // This needs to be done only once, ideally at app launch.
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                initializationDone = true;
+                Log.e(TAG, "initializationDone = true SEt");
+                loadAd();
+            }
+        });
+
+        /*
+            //set fullscreen callback
+            mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                @Override
+                public void onAdShowedFullScreenContent() {
+                    // Called when ad is shown.
+                    Log.e(TAG, "Ad was shown.");
+                }
+
+                @Override
+                public void onAdFailedToShowFullScreenContent(AdError adError) {
+                    // Called when ad fails to show.
+                    Log.e(TAG, "Ad failed to show.");
+                }
+
+                @Override
+                public void onAdDismissedFullScreenContent() {
+                    // Called when ad is dismissed.
+                    // Set the ad reference to null so you don't show the ad a second time.
+                    Log.e(TAG, "Ad was dismissed.");
+                    mRewardedAd = null;
+                }
+            });
+*/
+
+
+
+    }
+
+
+    void loadAd() {
+
+        // IF (){}
+        if (initializationDone == true) {
+            Log.e(TAG, "initializationDone == true");
+            //load rewarded ad object
+            AdRequest adRequest = new AdRequest.Builder().build();
+
+            RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                    adRequest, new RewardedAdLoadCallback() {
+                        @Override
+                        public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                            // Handle the error.
+                            Log.e(TAG, loadAdError.getMessage());
+                            mRewardedAd = null;
+                        }
+
+                        @Override
+                        public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                            mRewardedAd = rewardedAd;
+                            Log.e(TAG, "Ad was loaded.");
+                            showAd();
+                        }
+                    });
+
+
+        }
+    }
+
+    void showAd(){
+        //show the ad
+        Log.e(TAG, "Show Ad block started");
+        if (mRewardedAd != null) {
+            Log.e(TAG, "mRewardedAd != null inside \"show the ad\"");
+            Activity activityContext = MainActivity.this;
+            mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                    // Handle the reward.
+                    Log.e(TAG, "The user earned the reward.");
+                    int rewardAmount = rewardItem.getAmount();
+                    String rewardType = rewardItem.getType();
+                }
+            });
+        } else {
+            Log.e(TAG, "The rewarded ad wasn't ready yet.");
+        }
+    }
+}
